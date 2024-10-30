35cdab8246a4083db897b7d25e083b7690c01496
Sharif Alalem <sharif.alalem@savings-united.com>
2024-10-30 04:32:41 +0100

    apply marked logic & improve style

--- src/components/PreviewerComponent/Previewer.jsx
+++ src/components/PreviewerComponent/Previewer.jsx
@@ -1,17 +1,23 @@
 import { Component } from "react";
 import "./Previewer.scss";
+import { marked } from 'marked';
+
+
+marked.setOptions({
+    breaks: true, // Converts line breaks into <br> tags
+    // Additional options can go here
+  });
 
 class Previewer extends Component {
     constructor(props) {
         super(props);
     }
-    state = {  }
+    state = {  };
     render() { 
         return ( 
             <section className="preview-section">
-                 <h4>#Preview</h4>
-                <div id="preview">
-                    {this.props.markdown}          
+                <h4>#Preview</h4>
+                <div id="preview" dangerouslySetInnerHTML={{ __html: marked(this.props.markdown) }}>
                 </div>
             </section>
          );
